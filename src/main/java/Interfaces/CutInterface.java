30997d08713b33a99df6c19ecf5c6870cee22c6d
Demaemos <tylerandrews187@gmail.com>
2025-03-21 12:48:16 -0600

    Added all interfaces from prep into preppy

---
+++ src/main/java/Interfaces/CutInterface.java
@@ -0,0 +1,12 @@
+package Interfaces;
+
+public interface CutInterface {
+    public String slice(String IID);
+    public String chop(String IID);
+    public String dice(String IID);
+    public String mince(String IID);
+    public String shred(String IID);
+    public String ground(String IID);
+    public String julienne(String IID);
+
+}
---
+++ src/main/java/Interfaces/IIDComponent.java
@@ -0,0 +1,8 @@
+package Interfaces;
+
+public interface IIDComponent {
+    public Class<IIDComponent> getTypeEnum();
+
+    public String getComponentText();
+
+}
