850379b08959fae58874d37b45c0b8e04652a044
officialbohaus <110848971+officialbohaus@users.noreply.github.com>
2025-03-28 09:47:49 -0600

    Update IngredientSetInterface.java
    added expected functionality to IngredientSetInterface

--- src/main/java/Interfaces/IngredientSetInterface.java
+++ src/main/java/Interfaces/IngredientSetInterface.java
@@ -29,3 +29,16 @@ public interface IngredientSetInterface {
 
     public boolean contains(IIDContainer iidContainer);
 }
+
+/*
+    should hold multiple IIDContainers in a "locked" state
+        shouldn't allow incomplete IIDContainers, nor allow contained IIDContainers to be modified
+    should be able to provide a list of contained IIDContainers
+    should have a description, provided by user in constructor to define what the relation is between IIDContainers
+    should have a name defining the relationship (e.g. gluten-free grains)
+    should provide add method to add an IIDContainer
+    should provide remove method to remove an IIDContainer
+        these methods should not allow duplicate additions, or invalid removals
+        can target IDDContainer for removal by specifying name + descriptor tag (or providing an IIDContainer that matches)
+    should compare & list differences between two sets (see legacy code for example)
+ */
