74ced1c9c7026708bdaf73dcb0aaf966945acd2b
officialbohaus <110848971+officialbohaus@users.noreply.github.com>
2025-03-28 21:30:51 -0600

    further added 4 static methods to SetDifference for comparing two ingredient sets

--- src/main/java/Main/SetDifference.java
+++ src/main/java/Main/SetDifference.java
@@ -5,6 +5,7 @@ import Interfaces.IIDContainerInterface;
 import Interfaces.IngredientSetInterface;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * represents the differences between two <code>IngredientSet</code> objects. One object is the primarySet (likely
@@ -13,8 +14,8 @@ import java.util.ArrayList;
  *     uses four lists that represents the similarity of <code>IIDContainers</code> between two sets
  *     <p> <code>fullMatchList</code> - IIDContainers that occur identically in both sets
  *     <p> <code>partialMatchList</code> - IIDContainers from the contrastSet that match on name but differ by some other tag to an IIDContainer in the primarySet
- *     <p> <code>noMatchList</code> - IIDContainers from the contrastSet that do not match on any level some IIDContainer in the primarySet
- *     <p> <code>missingMatchList</code> - IIDContainers from the primarySet that did not have an exact matching IIDContainer in the contrastSet
+ *     <p> <code>noMatchInContrastList</code> - IIDContainers from the contrastSet that do not match on any level some IIDContainer in the primarySet
+ *     <p> <code>noMatchInPrimaryList</code> - IIDContainers from the primarySet that did not have an exact matching IIDContainer in the contrastSet
  */
 public class SetDifference {
 
@@ -47,12 +48,46 @@ public class SetDifference {
         return new IngredientSet(setName, setDesc, partialMatchList);
     }
 
-    private static ArrayList<IIDContainerInterface> getNoMatchList(IngredientSetInterface primarySet, IngredientSetInterface contrastSet) {
+    private static ArrayList<IIDContainerInterface> getNoMatchInContrastList(IngredientSetInterface primarySet, IngredientSetInterface contrastSet) {
+        ArrayList<IIDContainerInterface> noMatchInContrastList = new ArrayList<>();
+        for (IIDContainerInterface contrastIID : contrastSet.getIngredientSet()) {
+            Iterator<IIDContainerInterface> iterator = primarySet.getIngredientSet().iterator();
+            boolean foundAnyMatch = false;
+            while (iterator.hasNext() && !foundAnyMatch) {
+                IIDContainerInterface nextIID = iterator.next();
+                if (nextIID.isIID(contrastIID) || nextIID.getNameTag().equals(contrastIID.getNameTag())) {
+                    foundAnyMatch = true;
+                }
+            }
+            if (!foundAnyMatch) {
+                noMatchInContrastList.add(contrastIID);
+            }
+        }
 
+        String setName = "noMatchInContrastList " + primarySet.getName() + "/" + contrastSet.getName();
+        String setDesc = "noMatchInContrastList for primarySet \"" + primarySet.getName()  + "\" against contrastSet \"" + contrastSet.getName()  + "\"";
+        return new IngredientSet(setName, setDesc, noMatchInContrastList);
     }
 
-    private static ArrayList<IIDContainerInterface> getMissingMatchList(IIDContainerInterface primarySet, IIDContainerInterface contrastSet) {
+    private static ArrayList<IIDContainerInterface> getNoMatchInPrimaryList(IngredientSetInterface primarySet, IngredientSetInterface contrastSet) {
+        ArrayList<IngredientSetInterface> noMatchInPrimaryList = new ArrayList<>();
+        for (IIDContainerInterface primaryIID : primarySet.getIngredientSet()) {
+            Iterator<IIDContainerInterface> iterator = contrastSet.getIngredientSet().iterator();
+            boolean foundAnyMatch = false;
+            while (iterator.hasNext() && !foundAnyMatch) {
+                IIDContainerInterface nextIID = iterator.next();
+                if (nextIID.isIID(primaryIID) || nextIID.getNameTag().equals(primaryIID.getNameTag())) {
+                    foundAnyMatch = true;
+                }
+            }
+            if (!foundAnyMatch) {
+                noMatchInPrimaryList.add(primarySet);
+            }
+        }
 
+        String setName = "noMatchInPrimaryList " + primarySet.getName() + "/" + contrastSet.getName();
+        String setDesc = "noMatchInPrimaryList for primarySet \"" + primarySet.getName()  + "\" against contrastSet \"" + contrastSet.getName()  + "\"";
+        return new IngredientSet(setName, setDesc, noMatchInPrimaryList);
     }
 
     public SetDifference() {}
