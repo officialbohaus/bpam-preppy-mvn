168f285bbb759e6204d7e426049d0d6ad53d40ef
Demaemos <tylerandrews187@gmail.com>
2025-03-31 19:42:03 -0600

    added testing nearly all of the listed tests, except for getIIDStringTest(). Will need more time to fully build out last test.

--- src/test/java/IIDTests.java
+++ src/test/java/IIDTests.java
@@ -347,11 +347,14 @@ public class IIDTests {
         IIDContainer testContainer = getValidIIDContainer();
         testContainer.unlock();
         testContainer.addTag(CutState.GROUND);
+        assertTrue(testContainer.hasTag(CutState.GROUND));
         testContainer.addTag(CookState.RAW);
+        assertTrue(testContainer.hasTag(CookState.RAW));
         testContainer.addTag(IngredientUnit.GRAM);
+        assertTrue(testContainer.hasTag(IngredientUnit.GRAM));
         testContainer.addTag(IngredientType.PROTEIN);
+        assertTrue(testContainer.hasTag(IngredientType.PROTEIN));
         testContainer.lock();
-        assertTrue(testContainer.isValidIID());
     }
 
     @Test
@@ -391,7 +394,14 @@ public class IIDTests {
 
     @Test
     public void addTagsTest() {
-        fail("Not yet implemented");
+        IIDContainer testContainer = getValidIIDContainer();
+        testContainer.unlock();
+        IIDTag[] testList = {CutState.WHOLE, CookState.RAW};
+        ArrayList<IIDTag> testList2 = new ArrayList(Arrays.asList(testList));
+        testContainer.addTags(testList2);
+        testContainer.lock();
+        assertTrue(testContainer.hasTag(CutState.WHOLE));
+        assertTrue(testContainer.hasTag(CookState.RAW));
     }
 
     // </editor-fold>
